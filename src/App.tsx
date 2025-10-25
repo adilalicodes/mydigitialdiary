7ff012b391f713546382bea228eb46b5cfec164f
Adil Ali <adil.ali.codes@gmail.com>
2025-10-25 13:12:46 +0530

    to add the tailwind and shadcn configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button"
+
 function App() {
   return (
     <>
       <h1>Welcome to My Digital</h1>
+      <Button variant="outline">Save</Button>
     </>
   )
 }
